30aca1160869163ea6fa4cfa199a5be504a5ea8a
Azeem Cole <63126896+Azeem-Cole@users.noreply.github.com>
2021-11-23 01:26:33 -0500

    Update googlePARSE_test.py

--- google/googleSCRAPE/googlePARSE_test.py
+++ google/googleSCRAPE/googlePARSE_test.py
@@ -6,11 +6,6 @@ count = 0
 
 file = open("google/googleSCRAPE/googleTRENDING.txt", "r")
 
-import os
-
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
 
 for line in file:
     allTrend[count] = line.split(',')
